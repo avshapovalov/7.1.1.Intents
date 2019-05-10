c82fbe8d474cc9cf7ed0bc40e263fc1c7f7c4087
Alexander Shapovalov <avshapovalov90@mail.ru>
2019-05-10 13:01:36 +0300

    Initial commit

---
+++ app/src/main/java/com/example/intents/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.intents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void doSynchronization(View view) {
+        Intent intent = new Intent(Intent.ACTION_SYNC);
+        if (isCurrentTimeBetween(6,00, 14,00)){
+            intent.setData(Uri.parse("http://morning"));
+            startActivity(intent);
+        } else if (isCurrentTimeBetween(14,00, 15,00)) {
+            intent.setData(Uri.parse("http://afternoon"));
+            startActivity(intent);
+        } else if (isCurrentTimeBetween(14,00, 15,00)) {
+            intent.setData(Uri.parse("http://evening"));
+            startActivity(intent);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    private boolean isCurrentTimeBetween(int startHour, int startMinute, int endHour, int endMinute) {
+        ZoneId zoneId = ZoneId.of("Europe/Moscow");
+        ZonedDateTime zonedDateTime = ZonedDateTime.now(zoneId);
+        LocalTime localTime = zonedDateTime.toLocalTime();
+
+        LocalTime start = LocalTime.of(startHour, startMinute);
+        LocalTime stop = LocalTime.of(endHour, endMinute);
+
+        if (localTime.isAfter(start) && localTime.isBefore(stop)) {
+            return true;
+        } else if (stop.isBefore(start)) {  // Example 11 PM to 8 AM.
+            return false;
+        }
+        return false;
+    }
+
+
+}
